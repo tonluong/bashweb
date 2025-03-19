7dd8d6a2e5433ed889e4cd890d293eae103f06f6
Ton Luong <tonluong@gmail.com>
2025-03-19 15:03:01 -0700

    Update git.sh - add _git-me(), _git-me-work()

--- git.sh
+++ git.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# Set in local
+# _git-me-name, _git-me-email, _git-me-signingkey, _git-me-gpgsign
+# _git-me-work-name, _git-me-work-email, _git-me-work-signingkey, _git-me-work-gpgsign
+
 function _git() {
     alias g="git"
     git config --global alias.a 'add'
@@ -17,4 +21,18 @@ function _git() {
     git config --global alias.lgg "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)' --all"
     git config --global alias.r 'remote -v'
     git config --global alias.gl 'config --global -l'
+}
+
+function _git-me() {
+    git config --global user.name ${_git-me-name}
+    git config --global user.email ${_git-me-email}
+    git config --global user.signingkey ${_git-me-signingkey}
+    git config --global commit.gpgsign ${_git-me-gpgsign}
+}
+
+function _git-me-work() {
+    git config --global user.name ${_git-me-work-name}
+    git config --global user.email ${_git-me-work-email}
+    git config --global user.signingkey ${_git-me-work-signingkey}
+    git config --global commit.gpgsign ${_git-me-work-gpgsign}
 }
