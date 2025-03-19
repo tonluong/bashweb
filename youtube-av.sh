8be969ee1063a5fae33d7f2e023e3d213ac0085b
Ton Luong <tonluong@gmail.com>
2025-03-19 14:42:32 -0700

    refactor to youtube-av.sh

--- youtube-av.sh
+++ youtube-av.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # bloomberg, ABC-Australia, CNA, Al-Jazeera, France24English
-function _yt() {
+function _yt.av() {
     local _ytaudio=("iEpJwprxDdk" "vOTiJkg1voo" "XWq5kBlakcQ" "gCNeDWCI0vo" "Ap-UM1O9RBU")
     for ((i=1; i <= $#_ytaudio; i++)); do
         alias yta1.${i}="mpv --no-video --ytdl-format=91 ytdl://${_ytaudio[i]}"
